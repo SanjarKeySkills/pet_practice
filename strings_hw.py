dd4ff529b24bc26e3ae9794fce77a3a32707d316
SanjarKeySkills <forest2004@mail.ru>
2025-11-02 23:59:30 +0600

    auto commit Sun, Nov  2, 2025 11:59:30 PM

---
+++ strings_hw.py
@@ -0,0 +1,11 @@
+# Basic string exercises
+# Fill in the code for the functions below. main() is already set up
+# to call the functions with a few different inputs,
+# printing 'OK' when each function is correct.
+# The starter code for each function includes a 'return'
+# which is just a placeholder for your code.
+# It's ok if you do not complete all the functions, and there
+# are some additional functions to try in string2.py.
+
+
+
