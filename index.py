f8b046d7c20721d6c3c51660508267507354f924
SanjarKeySkills <forest2004@mail.ru>
2025-11-02 23:46:06 +0600

    auto commit Sun, Nov  2, 2025 11:46:05 PM

--- index.py
+++ index.py
@@ -9,7 +9,6 @@ from collections import Counter
 str = "If you want your classes to be reusable then you will find that different clients have different output requirements let the client not the class worry about the output side of things."
 
 list = str.split()
-print(list)
 
 object = Counter(list)
 x = dict(object)
