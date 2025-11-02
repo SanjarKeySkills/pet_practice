d18c6605d9416973ba506eafc94d2c01b608a25c
SanjarKeySkills <forest2004@mail.ru>
2025-11-03 00:05:04 +0600

    auto commit Mon, Nov  3, 2025 12:05:04 AM

--- index.py
+++ index.py
@@ -6,6 +6,8 @@
 
 
 from collections import Counter
+sentence = str(input("Please insert your sentence"))
+
 str = "If you want your classes to be reusable then you will find that different clients have different output requirements let the client not the class worry about the output side of things."
 
 list = str.split()
