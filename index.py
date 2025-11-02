c9eebfeafa650507d5791b12c72178ed101516c3
SanjarKeySkills <forest2004@mail.ru>
2025-11-02 23:44:09 +0600

    auto commit Sun, Nov  2, 2025 11:44:09 PM

--- index.py
+++ index.py
@@ -1,2 +1,14 @@
 def main():
     print("Hello World!")
+
+
+
+from collections import Counter
+str = "If you want your classes to be reusable then you will find that different clients have different output requirements let the client not the class worry about the output side of things."
+
+list = str.split()
+print(list)
+
+object = Counter(list)
+x = dict(object)
+print(x)
