9bfec1da844fe0750137a4da32ab12377e8e10d1
SanjarKeySkills <forest2004@mail.ru>
2025-11-05 21:40:38 +0600

    added algorythm

--- course.py
+++ course.py
@@ -2,5 +2,21 @@
 # y = 2
 # unit_price = 3
 
-print("Hello World!")
+# print("Hello World!")
 
+def calculate_sum(numbers):
+  """
+  Алгоритм для вычисления суммы всех чисел в списке.
+
+  :param numbers: Список чисел.
+  :return: Сумма чисел в списке.
+  """
+  total = 0  # Шаг 1: Инициализировать переменную для суммы
+  for number in numbers:  # Шаг 2: Повторять для каждого числа в списке
+    total += number  # Шаг 3: Прибавить текущее число к сумме
+  return total  # Шаг 4: Вернуть итоговую сумму
+
+# Пример использования
+my_list = [1, 2, 3, 4, 5]
+result = calculate_sum(my_list)
+print(f"Сумма элементов списка: {result}") # Вывод: Сумма элементов списка: 15
