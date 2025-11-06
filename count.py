0a2d1bb3d5d95162d41866a4143aad92bb4939a6
SanjarKeySkills <forest2004@mail.ru>
2025-11-06 17:44:46 +0600

    auto commit Thu, Nov  6, 2025  5:44:46 PM

--- count.py
+++ count.py
@@ -3,3 +3,7 @@ from collections import Counter
 my_list = ['apple', 'banana', 'apple', 'cherry', 'apple', 'banana']
 item_counts = len(my_list)
 print(item_counts)
+
+my_list1 = ['apple', 'banana', 'apple', 'cherry', 'apple', 'banana']
+item_counts = len(my_list)
+print(item_counts)
