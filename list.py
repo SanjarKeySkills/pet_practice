721393713275da76709c90be1fce7c5b96710b33
SanjarKeySkills <forest2004@mail.ru>
2025-11-07 22:59:55 +0600

    auto commit Fri, Nov  7, 2025 10:59:55 PM

--- list.py
+++ list.py
@@ -16,7 +16,7 @@ def find_maximum_in_list(numbers):
 
 # Example usage:
 # my_list = [3, 1, 4, 1, 5, 9, 2, 6]
-my_list = [12, 3, 4, 5, 6, 7, 8, 9, 13]
+my_list = [12, 3, 4, 5, 6, 7, 8, 9, 10.1]
 maximum = find_maximum_in_list(my_list)
 print(f"The maximum value in the list {my_list} is: {maximum}")
 
