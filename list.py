a4f0db431666d171b1f8f7c0c92f574f438ca1bc
SanjarKeySkills <forest2004@mail.ru>
2025-11-08 16:29:57 +0600

    auto commit Sat, Nov  8, 2025  4:29:57 PM

--- list.py
+++ list.py
@@ -37,6 +37,6 @@ def find_minimum_in_list(numbers):
             print(number)
     return min_value
     
-min_list = [1, 18, 14, 13, 14, 15, 6, 5]
+min_list = [1, 18, 14, 13, 14, 15, 0.6, 5]
 minimum = find_minimum_in_list(min_list)
 print(f"The minmum value in the list {min_list} is: {minimum}")
