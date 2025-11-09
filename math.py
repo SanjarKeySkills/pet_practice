ffb21c523e06b455328f476ded7da151e94b2f7c
SanjarKeySkills <forest2004@mail.ru>
2025-11-09 23:21:56 +0600

    auto commit Sun, Nov  9, 2025 11:21:55 PM

--- math.py
+++ math.py
@@ -6,8 +6,9 @@ set_B = {3, 4, 5, 6}
 union_set = set_A.union(set_B)  # {1, 2, 3, 4, 5, 6}
 intersection_set = set_A.intersection(set_B) # {3, 4}
 
-
 # ----------------------------
+
+
 
 import itertools
     perms = list(itertools.permutations([1, 2, 3])) # [(1, 2, 3), (1, 3, 2), ...]
