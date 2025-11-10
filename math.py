75768bb1c654e5f8a357dea7feb0d2810124ccbc
SanjarKeySkills <forest2004@mail.ru>
2025-11-10 20:50:06 +0600

    auto commit Mon, Nov 10, 2025  8:50:06 PM

--- math.py
+++ math.py
@@ -6,14 +6,4 @@ set_B = {3, 4, 5, 6}
 union_set = set_A.union(set_B)  # {1, 2, 3, 4, 5, 6}
 intersection_set = set_A.intersection(set_B) # {3, 4}
 
-# ----------------------------
-# Combinatorics: The itertools module provides functions like permutations, 
-# combinations, and product for generating and analyzing combinatorial objects.
 
-import itertools
-    perms = list(itertools.permutations([1, 2, 3])) # [(1, 2, 3), (1, 3, 2), ...]
-    combs = list(itertools.combinations([1, 2, 3, 4], 2)) # [(1, 2), (1, 3), ...]
-
-# ----------------------------
-# Graph Theory: Libraries like NetworkX offer extensive functionalities for creating,
-# manipulating, and analyzing graphs, including algorithms for shortest paths, connectivity, and centrality measures.
