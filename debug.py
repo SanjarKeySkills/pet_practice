96939de7376deb9559c1a54f611ff84de121ecfe
SanjarKeySkills <forest2004@mail.ru>
2025-11-12 23:26:18 +0600

    auto commit Wed, Nov 12, 2025 11:26:18 PM

--- debug.py
+++ debug.py
@@ -1,11 +1,13 @@
 a = 33
 b = 200
-if a < b:
+# if a < b:
 	# print("b is bigger than a")
  
 # ----------------------
-num = 180
+
 first = 10
 second = 20
+num = 180
+
 if first + second > num:
     print("equation is not mine")
