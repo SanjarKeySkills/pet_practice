c9585a2886138ef2341e15ed7acb24b78742e1fe
SanjarKeySkills <forest2004@mail.ru>
2025-11-13 22:24:45 +0600

    auto commit Thu, Nov 13, 2025 10:24:45 PM

--- count.py
+++ count.py
@@ -4,6 +4,6 @@ my_list = ['apple', 'banana', 'apple', 'cherry', 'apple', 'banana']
 item_counts = len(my_list)
 print(item_counts)
 
-my_list1 = ['apple', 'banana', 'apple', 'cherry', 'apple', 'banana']
-item_counts = len(my_list)
-print(item_counts)
+# my_list1 = ['apple', 'banana', 'apple', 'cherry', 'apple', 'banana']
+# item_counts = len(my_list)
+# print(item_counts)
