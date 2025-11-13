257a997c24f7d9a686892a971e61f5b94b635cce
SanjarKeySkills <forest2004@mail.ru>
2025-11-13 22:45:41 +0600

    auto commit Thu, Nov 13, 2025 10:45:41 PM

--- initial.py
+++ initial.py
@@ -187,15 +187,15 @@ print(result)
 # main()
 
 # ------------------------------------------------
-from collections import Counter
-str = "If you want your classes to be reusable then you will find that different clients have different output requirements let the client not the class worry about the output side of things."
+# from collections import Counter
+# str = "If you want your classes to be reusable then you will find that different clients have different output requirements let the client not the class worry about the output side of things."
 
-list = str.split()
-print(list)
+# list = str.split()
+# print(list)
 
-object = Counter(list)
-x = dict(object)
-print(x)
+# object = Counter(list)
+# x = dict(object)
+# print(x)
 
 # ------------------
 a = 90
