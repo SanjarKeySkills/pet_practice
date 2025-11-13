26937630418333785a1cc17c1b3a8f0cab91330a
SanjarKeySkills <forest2004@mail.ru>
2025-11-13 23:04:55 +0600

    auto commit Thu, Nov 13, 2025 11:04:55 PM

--- loop.py
+++ loop.py
@@ -38,8 +38,8 @@
 
 # -----------------
 
-for x in range(1, 21):
-    if x == 13:
-        break
-    else:
-        print(x)
+# for x in range(1, 21):
+#     if x == 13:
+#         break
+#     else:
+#         print(x)
