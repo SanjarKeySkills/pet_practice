835ad7dbc100d5694f20d459b6b30a6cddbc7adb
SanjarKeySkills <forest2004@mail.ru>
2025-11-14 23:23:53 +0600

    auto commit Fri, Nov 14, 2025 11:23:53 PM

--- testing/next.py
+++ testing/next.py
@@ -2,7 +2,7 @@ import unittest
 import test
 
 class TestCalc(unittest.TestCase):
-    
     def test_add(self):
         self.assertEqual(test.add(10, 5), 15)
         self.assertEqual(test.add(-1, 1), 0)
+        self.assertEqual(test.add(10, 9), 19)
