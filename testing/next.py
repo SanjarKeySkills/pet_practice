28a7cb93fb6f65658dce86f7faade709dd0117e6
SanjarKeySkills <forest2004@mail.ru>
2025-11-16 23:12:12 +0600

    deketed exceeded data

--- testing/next.py
+++ testing/next.py
@@ -12,15 +12,5 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(test.add(-1, 1), 0)
         self.assertEqual(test.add(10, 9), 19)
         
-    def test_add(self):
-	    self.assertEqual(test.add(10, 5), 15)
-        self.assertEqual(test.add(-1, 1), 0)
-        self.assertEqual(test.add(10, 9), 19)
-        
-    def test_add(self):
-	    self.assertEqual(test.add(10, 5), 15)
-        self.assertEqual(test.add(-1, 1), 0)
-        self.assertEqual(test.add(10, 9), 19)
-
 if __name__ == '__main__':
     unittest.main()
