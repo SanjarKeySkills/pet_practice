0035637cbca0b36c02086ad14562dc6992bb1e3e
SanjarKeySkills <forest2004@mail.ru>
2025-11-18 23:58:18 +0600

    added exchange rate app

--- temperature.py
+++ temperature.py
@@ -1,3 +1,18 @@
-farenheit = float(input("please isert your temperature"))
-celsius = (5/9) * (farenheit - 32)
-print(f"In Celsium it is: {celsius:.2f}")
+# farenheit = float(input("please isert your temperature"))
+# celsius = (5/9) * (farenheit - 32)
+# print(f"In Celsium it is: {celsius:.2f}")
+
+
+
+
+# --------------------
+
+
+# Конвертация USD -> другая валюта (например, KGS)
+
+amount_usd = float(input("Введите сумму в долларах США: "))
+rate = float(input("Введите курс доллара (сколько стоит 1 USD): "))
+
+result = amount_usd * rate
+
+print("Результат перевода:", result)
