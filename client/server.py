1e10f856d106210c82a9db5ac368b6c2058ea13e
SanjarKeySkills <forest2004@mail.ru>
2025-11-21 10:58:04 +0600

    auto commit Fri, Nov 21, 2025 10:58:04 AM

--- client/server.py
+++ client/server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 
+HEDER = 64 # 64 tells msg that is going to come next Header = 64 symbols
 PORT = 5050 # SERVER = "192.168.31.127" на этом порте сервер будет слушать входящие сообщения
 SERVER = socket.gethostbyname(socket.gethostname()) #gethostname - полуаем имя компьютера
 # gethostbyname - получаем по имени IP локальные адреса
