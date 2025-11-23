dbcb9c449c9fe9d2e64ced4a0e861ad2bef5dfe2
SanjarKeySkills <forest2004@mail.ru>
2025-11-24 00:30:58 +0600

    auto commit Mon, Nov 24, 2025 12:30:58 AM

--- client/server.py
+++ client/server.py
@@ -29,7 +29,7 @@ def handle_client(conn, addr):
 					connected = False
 			
    			print(f"[{addr}] {msg}") # handling the disconnection clearly
-		conn.close() #closed disconnection
+			conn.close() #closed disconnection
 
 
 
