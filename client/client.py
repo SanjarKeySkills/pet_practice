12b648cb3ba77009b488656a439f6d3ac2d54913
SanjarKeySkills <forest2004@mail.ru>
2025-11-23 23:53:43 +0600

    auto commit Sun, Nov 23, 2025 11:53:43 PM

--- client/client.py
+++ client/client.py
@@ -3,7 +3,7 @@ import socket
 HEADER = 64 # 64 tells msg that is going to come next Header = 64 symbols
 PORT = 5050 # SERVER = "192.168.31.127" на этом порте сервер будет слушать входящие сообщения
 SERVER = socket.gethostbyname(socket.gethostname()) #gethostname - полуаем имя компьютера
-# gethostbyname - получаем по имени IP локальные адреса
+
 ADDR = (SERVER, PORT) # tuple
 FORMAT = 'UTF-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
