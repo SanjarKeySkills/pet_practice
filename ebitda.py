6854ce80d547b25268e5fe1fa67736bab289ba55
SanjarKeySkills <forest2004@mail.ru>
2025-11-26 22:08:36 +0600

    auto commit Wed, Nov 26, 2025 10:08:36 PM

--- ebitda.py
+++ ebitda.py
@@ -34,3 +34,48 @@ ebitda = calculate_ebitda_from_ebit(ebit, depreciation, amortization)
 print(f"EBITDA: {ebitda:,.2f}")
 
 # ------------------------------------------------
+# ООП подход с классом Company
+class Company:
+    def __init__(self, name, revenue, cogs, operating_expenses, depreciation, amortization):
+        self.name = name
+        self.revenue = revenue
+        self.cogs = cogs
+        self.operating_expenses = operating_expenses
+        self.depreciation = depreciation
+        self.amortization = amortization
+    
+    @property
+    def ebit(self):
+        """Операционная прибыль (EBIT)"""
+        return self.revenue - self.cogs - self.operating_expenses
+    
+    @property
+    def ebitda(self):
+        """EBITDA"""
+        return self.ebit + self.depreciation + self.amortization
+    
+    def financial_report(self):
+        """Генерация финансового отчета"""
+        return f"""
+Финансовый отчет: {self.name}
+Выручка: {self.revenue:,.2f}
+Себестоимость: {self.cogs:,.2f}
+Операционные расходы: {self.operating_expenses:,.2f}
+EBIT: {self.ebit:,.2f}
+Амортизация ОС: {self.depreciation:,.2f}
+Амортизация НМА: {self.amortization:,.2f}
+EBITDA: {self.ebitda:,.2f}
+"""
+
+# Использование
+company = Company(
+    name="ТехноКорп",
+    revenue=1500000,
+    cogs=800000,
+    operating_expenses=300000,
+    depreciation=60000,
+    amortization=40000
+)
+
+print(company.financial_report())
+
