ccea6f5924dcc03caeaac46436dd1f555f508b93
SanjarKeySkills <forest2004@mail.ru>
2025-11-26 22:03:00 +0600

    auto commit Wed, Nov 26, 2025 10:03:00 PM

--- math.py
+++ math.py
@@ -1,6 +1,9 @@
 # Set Theory: Python's built-in set data type directly supports operations like union, 
 # ntersection, difference, and symmetric difference, making it easy to model and manipulate sets.
 
+
+
+
 set_A = {1, 2, 3, 4}
 set_B = {3, 4, 5, 6}
 union_set = set_A.union(set_B)  # {1, 2, 3, 4, 5, 6}
