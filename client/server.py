57c43e6bf031f2695e1c6456c6ace5dacfdcf5ab
SanjarKeySkills <forest2004@mail.ru>
2025-11-30 16:19:19 +0600

    auto commit Sun, Nov 30, 2025  4:19:19 PM

--- client/server.py
+++ client/server.py
@@ -25,9 +25,8 @@ def handle_client(conn, addr):
 				msg_length = int(msg_length)
 				msg_length = conn.recv(msg_length).decode(FORMAT)
 				if msg == DISCONNECT_MESSAGE:
-					connected = False
-			
-   					print(f"[{addr}] {msg}") # handling the disconnection clearly
+				connected = False
+				print(f"[{addr}] {msg}") # handling the disconnection clearly
 			conn.close() #closed disconnection
 
 def start():
