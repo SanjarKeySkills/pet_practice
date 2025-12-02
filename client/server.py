76da5b4a4eb72cede02106549c903d5cac61a2d9
SanjarKeySkills <forest2004@mail.ru>
2025-12-02 21:04:04 +0600

    auto commit Tue, Dec  2, 2025  9:04:04 PM

--- client/server.py
+++ client/server.py
@@ -12,6 +12,7 @@ FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
 server.bind(ADDR)
 
 def handle_client(conn, addr):
