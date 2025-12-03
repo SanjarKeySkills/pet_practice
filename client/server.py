c8bd0a2f9c91b78e78a19525243628d002dc1557
SanjarKeySkills <forest2004@mail.ru>
2025-12-03 20:21:14 +0600

    auto commit Wed, Dec  3, 2025  8:21:14 PM

--- client/server.py
+++ client/server.py
@@ -63,27 +63,6 @@ class HTTPRequest:
 
     
     
-    = self.parse_query_params(path_parts[1])
-			i = 1
-
- 
-def handle_client(conn, addr):
-    # handle communication between client and server
-    print(f"[NEW CONNECTION] {addr} connected.")
-    
-    while True:
-        connected = True
-        while connected:
-            #decode this function from its bite format to str using utf-8 format
-			msg_length = conn.recv(HEADER).decode(FORMAT)
-			#how many bites we are going to receive
-			if msg_length:
-				msg_length = int(msg_length)
-				msg_length = conn.recv(msg_length).decode(FORMAT)
-				if msg == DISCONNECT_MESSAGE:
-				connected = False
-				print(f"[{addr}] {msg}") # handling the disconnection clearly
-			conn.close() #closed disconnection
 
 def start():
     pass
