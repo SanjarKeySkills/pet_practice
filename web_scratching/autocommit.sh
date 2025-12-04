54eb206fbb786481aee0b09b47540f9dde862ed7
SanjarKeySkills <forest2004@mail.ru>
2025-12-04 23:35:31 +0600

    added autocommit.sh

---
+++ web_scratching/autocommit.sh
@@ -0,0 +1,11 @@
+echo "Control all changes in $PATH..."
+
+while true; do
+	if ! git diff --quiet || ! git diff --cached --quiet; then
+		echo "Changes defined! Commit and push..."
+		git add .
+		git commit -m "auto commit $(date)" || true
+		git push
+	fi
+	sleep 5
+done
---
+++ web_scratching/index.py
@@ -0,0 +1,15 @@
+
+
+def start(phrase):
+    x = "Hellow world!"
+    print("I jsut say: %s"% (phrase))
+
+start("Hello world")
+
+
+def start(phrase):
+    x = "Hellow world!"
+    print("I jsut say: %s"% (phrase))
+
+start("Hello world")
+
