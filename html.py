5e1e7c6225383b2598ed7c33f32fc33a09bb8359
SanjarKeySkills <forest2004@mail.ru>
2025-12-07 22:38:39 +0600

    training with html code

---
+++ html.py
@@ -0,0 +1,15 @@
+header = "<h1>This is Python!</h1>"
+paragraph = "<p>THis is param</p>"
+bold_text = "<strong>Bold text man!</strong> text."
+
+html_content = f"""
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello not World!</title>
+ </head>
+ <body>
+	{header}
+	{paragraph} {bold_text}
+ </body>
+"""
