b80a02b05b52336dc17b1acaff570acdd01a67dc
SanjarKeySkills <forest2004@mail.ru>
2025-12-18 23:50:18 +0600

    auto commit Thu, Dec 18, 2025 11:50:18 PM

--- math/main.py
+++ math/main.py
@@ -2,3 +2,5 @@
 #write the function  which receives array of numbers and return true, if at leat one duplicate will be in array and put
 # 'false' in opposite case
 #[1, 2, 3, 4, 5] -> false
+#[1, 2, 3, 2, 4] -> (2 num duplicates)
+
