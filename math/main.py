ca4a85d87e2573f8ee7d2f2c035aac326217ed36
SanjarKeySkills <forest2004@mail.ru>
2025-12-18 23:49:34 +0600

    auto commit Thu, Dec 18, 2025 11:49:34 PM

---
+++ math/main.py
@@ -0,0 +1,4 @@
+# Big O notation understanding task
+#write the function  which receives array of numbers and return true, if at leat one duplicate will be in array and put
+# 'false' in opposite case
+#[1, 2, 3, 4, 5] -> false
