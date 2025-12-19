18269a91b8a63b8c16474f1a76ef65c007488a67
SanjarKeySkills <forest2004@mail.ru>
2025-12-19 21:58:14 +0600

    auto commit Fri, Dec 19, 2025  9:58:14 PM

--- math/main.py
+++ math/main.py
@@ -65,6 +65,6 @@ def python_sort_example(arr):
     return sorted(arr)  # O(N log N)
 
 # Пример
-numbers = [5, 2, 9, 1, 5, 6, 8, 11, 23, 3]
+numbers = [5, 2, 9, 1, 5, 6, 8, 11, 4, 23, 3]
 sorted_numbers = python_sort_example(numbers)
 print(f"Сортировка через sorted(): {sorted_numbers}")
