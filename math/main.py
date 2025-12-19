f85cdf896ce899af09f5d50d6d47d869568dacc6
SanjarKeySkills <forest2004@mail.ru>
2025-12-19 21:36:21 +0600

    auto commit Fri, Dec 19, 2025  9:36:21 PM

--- math/main.py
+++ math/main.py
@@ -14,7 +14,8 @@ def has_duplicate_brute_force(nums):
 # Big O: O(n^2)
 
 # Merge Sort
-def merge_sort(arr):     """Сортировка слиянием - классический пример O(N log N)"""
+def merge_sort(arr): 
+    """Сортировка слиянием - классический пример O(N log N)"""
     if len(arr) <= 1:
         return arr
     
