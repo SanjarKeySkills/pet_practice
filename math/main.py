7fc2d21e303c078e6b149faf0cd8e88ee330eddc
SanjarKeySkills <forest2004@mail.ru>
2025-12-19 21:51:40 +0600

    auto commit Fri, Dec 19, 2025  9:51:40 PM

--- math/main.py
+++ math/main.py
@@ -4,13 +4,13 @@
 #[1, 2, 3, 4, 5] -> false
 #[1, 2, 3, 2, 4] -> (2 num duplicates)
 
-def has_duplicate_brute_force(nums):
-    n = len(nums)
-    for i in range(n):
-        for j in range(i + 1, n):
-            if nums[i] == nums[j]:
-                return True
-    return False
+# def has_duplicate_brute_force(nums):
+#     n = len(nums)
+#     for i in range(n):
+#         for j in range(i + 1, n):
+#             if nums[i] == nums[j]:
+#                 return True
+#     return False
 # ------------------------------------
 # Big O: O(n^2)
 
